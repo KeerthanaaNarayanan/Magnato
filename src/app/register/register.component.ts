239ee218b1b390a4ad0965ed17b3cbf275b67d61
knarayanan <knarayanan@sonicwall.com>
2021-04-21 16:31:13 +0530

    completed all the required functionalities

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,16 +1,67 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private dataservice: DataService) { }
+  create_user_url = this.dataService.base_url + 'create-user';
+  createForm: FormGroup;
+  requestOptions: Object = {
+    responseType: 'text',
+  };
+  reponse_content;
 
-  ngOnInit(): void {
-    
+  constructor(
+    public dataService: DataService,
+    public formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.createForm = this.formBuilder.group({
+      firstname: [''],
+      lastname: [''],
+      email: [''],
+      password: [''],
+    });
   }
 
+  ngOnInit(): void {}
+  notify(response) {
+    if (response == 'User saved successfully') {
+      alert(response);
+    } else {
+      alert(response);
+    }
+  }
+
+  createUser() {
+    this.http
+      .post<any>(
+        this.create_user_url,
+        {
+          firstname: this.createForm.get('firstname')?.value,
+          lastname: this.createForm.get('lastname')?.value,
+          email: this.createForm.get('email')?.value,
+          password: this.createForm.get('password')?.value,
+        },
+        this.requestOptions
+      )
+      .subscribe(
+        (data) => {
+          this.reponse_content = data;
+          this.notify(this.reponse_content);
+          console.log(this.reponse_content);
+        },
+        (error: any) => {
+          this.reponse_content = error;
+          this.notify(this.reponse_content);
+          console.log(this.reponse_content);
+        }
+      );
+  }
 }
+
--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
+import { HttpClient } from '@angular/common/http';
+import {
+  FormBuilder,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-create',
@@ -7,23 +12,59 @@ import { DataService } from '../data.service';
   styleUrls: ['./user-create.component.css'],
 })
 export class UserCreateComponent implements OnInit {
-  contact: { id; email; firstname; lastname; password; designation; application; } = {
-    id: null,
-    email: '',
-    firstname: '',
-    lastname: '',
-    password: '',
-    designation: null,
-    application: null,
+  create_user_url = this.dataService.base_url + 'create-user';
+  createForm: FormGroup;
+  requestOptions: Object = {
+    responseType: 'text',
   };
+  reponse_content;
 
-  constructor(public dataService: DataService) {}
+  constructor(
+    public dataService: DataService,
+    public formBuilder: FormBuilder,
+    private http: HttpClient
+  ) {
+    this.createForm = this.formBuilder.group({
+      firstname: [''],
+      lastname: [''],
+      email: [''],
+      password: [''],
+    });
+  }
 
   ngOnInit() {}
 
-  createContact() {
-    console.log(this.contact);
-    this.dataService.createUser(this.contact);
-    this.contact = { id:null, email: '', firstname: '', lastname: '', password: '', designation: null, application: null };
+  notify(response) {
+    if (response == 'User saved successfully') {
+      alert(response);
+    } else {
+      alert(response);
+    }
+  }
+
+  createUser() {
+    this.http
+      .post<any>(
+        this.create_user_url,
+        {
+          firstname: this.createForm.get('firstname')?.value,
+          lastname: this.createForm.get('lastname')?.value,
+          email: this.createForm.get('email')?.value,
+          password: this.createForm.get('password')?.value,
+        },
+        this.requestOptions
+      )
+      .subscribe(
+        (data) => {
+          this.reponse_content = data;
+          this.notify(this.reponse_content);
+          console.log(this.reponse_content);
+        },
+        (error: any) => {
+          this.reponse_content = error;
+          this.notify(this.reponse_content);
+          console.log(this.reponse_content);
+        }
+      );
   }
 }
